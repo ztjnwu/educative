4238ebc2a26985a03fe0b7b186f9fa7da168ca31
ztjnwu <ztj_nwu@snnu.edu.cn>
2022-11-05 00:37:20 +0000

    11.4

--- BFS.java
+++ BFS.java
@@ -2,8 +2,99 @@ import java.util.*;
 
 public class BFS {
 
-    public static List<List<TreeNode>> traverse(BST bst)
+    //traverse a bst in layer
+    public static List<List<Integer>> traverse(BST bst)
     {
+        //Check validity
+        if(bst == null)
+        {
+            return null;
+        }
+
+        //Initialization
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(bst.getRoot());
+
+        //build the set result
+        while(queue.size() != 0)
+        {
+            int queueLength = queue.size();
+            List<Integer> tempList = new ArrayList<>();
+            for(int i = 0; i < queueLength; i++)
+            {
+                TreeNode tempNode = queue.poll(); //dequeue
+                 //enqueue left child
+                if(tempNode.left != null)
+                {
+                    queue.add(tempNode.left);
+                }
+                
+                //enqueue right child
+                if(tempNode.right != null)
+                {
+                    queue.add(tempNode.right);
+                }
+
+                //enlist
+                tempList.add(tempNode.value);
+            }//
+            
+            //Update the set result.
+            result.add(tempList);          
+        }
+
+        //return
+        return result;
+
+    }//
+
+    //reverse the result of traversing a bst in layer
+    public static List<List<Integer>> reverseTraverse(BST bst)
+    {
+        //Chcek validty
+        if(bst == null)
+        {
+            return null;
+        }
+
+        //Initialization
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(bst.getRoot());
+        
+        while(queue.size() != 0)
+        {
+            int queueLength = queue.size();
+            List<Integer> tempList = new ArrayList<>();
+            for(int i = 0; i < queueLength; i++)
+            {
+                TreeNode tempNode = queue.poll();
+                if(tempNode.left != null)
+                {
+                    queue.add(tempNode.left);
+                }
+                
+                if(tempNode.right != null)
+                {
+                    queue.add(tempNode.right);
+                }
+                
+                tempList.add(tempNode.value);
+            }
+
+            //update result
+            result.add(0, tempList);
+        }
+
+        //return
+        return result;
+
+    }
+
+    public static List<List<Integer>> zigzagTraverse(BST bst)
+    {
+        //check validity
         if(bst == null)
         {
             return null;
@@ -12,24 +103,51 @@ public class BFS {
         //
         List<List<Integer>> result = new ArrayList<>();
         Queue<TreeNode> queue = new LinkedList<>();
-        TreeNode root = bst.getRoot();
-        TreeNode p = root;
-        List<Integer> temp = result.get(0);
+        queue.add(bst.getRoot());
+        int level = 1;
         while(queue.size() != 0)
         {
-            queue.poll(); //dequeue 
-            
-            temp.add(p.value);
-        }
+            List<Integer> tempList = new ArrayList<>();
+            int length = queue.size();
+            for(int i = 0; i < length; i++)
+            {
+                TreeNode tempNode = queue.poll();
+                if(tempNode.left != null)
+                {
+                    queue.add(tempNode.left);
+                }
+                if(tempNode.right != null)
+                {
+                    queue.add(tempNode.right);
+                }
+
+                if(level % 2 == 0)
+                {
+                    tempList.add(0, tempNode.value);
+                }
+                else 
+                {
+                    tempList.add(tempNode.value);
+                }//else
+
+            }//for
+
+            //update the set result
+            result.add(tempList);
+            level++;
+
+        }//while
+
+        //return
+        return result;
 
     }//
 
-    
     public static void main(String[] args)
     {
         //Traverse a BST
-        System.out.println("Traverse a BST in preorder, inorde and postorder.");
-        BST bst = new BST(new int[] {1, 2, 3, 4, 5, 6, 7, 8});
+        System.out.println("Traverse a BST in preorder, inorder and postorder.");
+        BST bst = new BST(new int[] {6, 4, 2, 1, 3, 5, 8, 7, 9});
         bst.preOrder(bst.getRoot());
         System.out.println();
         bst.inOrder(bst.getRoot());
@@ -38,7 +156,20 @@ public class BFS {
         System.out.println();
 
         //Breath First Search 
+        System.out.println("\nTraverse a BST in BST.");
+        bst = new BST(new int[] {6, 4, 2, 1, 3, 5, 8, 7, 9});
+        System.out.println("Level order traversal: " + traverse(bst));
 
+        //Reverse BFS
+        System.out.println("\nTraverse a BST in BST.");
+        bst = new BST(new int[] {6, 4, 2, 1, 3, 5, 8, 7, 9});
+        System.out.println("Level order traversal: " + reverseTraverse(bst));
+
+        //Zigzag a BST
+        System.out.println("\nZigzag a BST in BST.");
+        bst = new BST(new int[] {6, 4, 2, 1, 3, 5, 8, 7, 9});
+        System.out.println("Level order traversal: " + zigzagTraverse(bst));
+        
     }
     
 }//
@@ -118,7 +249,6 @@ class BST {
 
     }//
 
-
     //Traverse a BST in preorder
     public void preOrder(TreeNode root)
     {
@@ -133,7 +263,6 @@ class BST {
         preOrder(root.right);
         
     }//
-
 
     //Traverse a BST in inorder
     public void inOrder(TreeNode root)
