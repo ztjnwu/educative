b393d24c2e12dadf3820d8d3011bb10b0dc9b164
ztjnwu <ztj_nwu@snnu.edu.cn>
2022-11-09 19:45:31 +0000

    11.9

--- KLargestNumbers.java
+++ KLargestNumbers.java
@@ -1,21 +1,50 @@
 import java.util.*;
 
 public class KLargestNumbers {
-    pubic static List<Integer> findKLargestNumbers(int[] arr, int K)
+    public static List<Integer> findKLargestNumbers(int[] arr, int K)
     {
         if(arr == null || arr.length <= 0)
         {
             return null;
         }
 
-        //
-        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
+        //Initialization
+        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>(Comparator.naturalOrder());
+        for(int i = 0; i < K; i++)
+        {
+            minHeap.add(arr[i]);
+        }
+
+        //Find the K largest numbers
+        for(int j = K; j < arr.length; j++)
+        {
+            if(arr[j] > minHeap.peek())
+            {
+                minHeap.poll();
+                minHeap.add(arr[j]);
+            }
+        }
+
+        //return
+        List<Integer> result = new ArrayList<>(minHeap); 
+        return result;
     }
 
     //Find K largest numbers
-    List<Integer> result = KLargestNumbers.findKLargestNumbers(new int[] { 3, 1, 5, 12, 2, 11 }, 3);
-    System.out.println("Here are the top K numbers: " + result);
-    result = KLargestNumbers.findKLargestNumbers(new int[] { 5, 12, 11, -1, 12 }, 3);
-    System.out.println("Here are the top K numbers: " + result);
+    public static void main(String[] argv)
+    {
+        //Find the largest numbers
+        ist<Integer> result = KLargestNumbers.findKLargestNumbers(new int[] { 3, 1, 5, 12, 2, 11 }, 3);
+        System.out.println("Here are the top K numbers: " + result);
+        result = KLargestNumbers.findKLargestNumbers(new int[] { 5, 12, 11, -1, 12 }, 3);
+        System.out.println("Here are the top K numbers: " + result);
+    }
+
+
+
+
+
+
+
 
 }
