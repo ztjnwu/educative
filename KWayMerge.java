e123c75d892b5af64b741a1038bf06f96cd37871
ztjnwu <ztj_nwu@snnu.edu.cn>
2022-11-13 21:21:37 +0000

    11.13

--- KWayMerge.java
+++ KWayMerge.java
@@ -1,8 +1,9 @@
 import java.util.*;
 
 public class KWayMerge {
-    public static ListNode mergeKWayList(ListNode[] lists)
+    public static ListNode mergeKWayList(List<List<ListNode>> lists)
     {
+        //
         if(lists == null)
         {
             return null;
@@ -12,9 +13,9 @@ public class KWayMerge {
         PriorityQueue<ListNode> minHeap = new PriorityQueue<ListNode>((a, b) -> a.value - b.value);
 
         //Initialization
-        for(int i = 0; i < lists.length; i++)
+        for(int i = 0; i < lists.size(); i++)
         {
-            minHeap.add(lists[i]);
+            minHeap.add(lists.get(i).get(0));
         }
         
         //Merge
@@ -51,22 +52,67 @@ public class KWayMerge {
 
     }//public
 
+    public static int findKthsmallestInManyList(List<List<Integer>> lists, int K)
+    {
+        //
+        if(lists == null)
+        {
+            return -1;
+        }
+
+        //
+        PriorityQueue<Node> minHeap = new PriorityQueue<Node>((a, b) -> a.value - b.value);
+        for(int i = 0; i < lists.size(); i++)
+        {
+            minHeap.add(new Node(i, 0, lists.get(i).get(0)));
+        }
+        
+        //
+        int num = 0;
+        Node temp = null;
+        while(!minHeap.isEmpty() && num != K )
+        {
+            temp = minHeap.poll();
+            num++;
+            if(temp.colum + 1 < lists.get(temp.row).size())
+            {
+                int row = temp.row;
+                int colum = temp.colum + 1;
+                int value = lists.get(temp.row).get(temp.colum + 1);
+                minHeap.add(new Node(row, colum, value));
+            }
+            
+        }//
+
+        //return
+        int result = temp.value;
+        return result;
+
+    }
 
     public static void main(String[] argv)
     {
+        //Merger many lists
+        System.out.println("Merge many lists");
+
         ListNode l1 = new ListNode(2);
         l1.next = new ListNode(6);
         l1.next.next = new ListNode(8);
+        List<ListNode> a = Arrays.asList(l1, l1.next, l1.next.next);
 
         ListNode l2 = new ListNode(3);
         l2.next = new ListNode(6);
         l2.next.next = new ListNode(7);
+        List<ListNode> b = Arrays.asList(l2, l2.next, l2.next.next);
 
         ListNode l3 = new ListNode(1);
         l3.next = new ListNode(3);
         l3.next.next = new ListNode(4);
+        List<ListNode> c = Arrays.asList(l3, l3.next, l3.next.next);
 
-        ListNode result = KWayMerge.mergeKWayList(new ListNode[] {l1, l2, l3});
+        List<List<ListNode>> lists = Arrays.asList(a, b, c);
+
+        ListNode result = KWayMerge.mergeKWayList(lists);
         ListNode temp = result;
         while(temp != null)
         {
@@ -74,7 +120,23 @@ public class KWayMerge {
             temp = temp.next;
         }//while
 
+        System.out.println("\n");
+
+        //
+        System.out.println("Find Kth smallest item" );
+        List<Integer> l4 = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> l5 = Arrays.asList(6, 7, 8, 9, 10);
+        List<Integer> l6 = Arrays.asList(11, 12, 13, 14, 15);
+        List<List<Integer>> lists_integer = Arrays.asList(l4, l5, l6);
+        System.out.println(lists_integer);
+
+        int result_int = KWayMerge.findKthsmallestInManyList(lists_integer, 15);
+        System.out.print("Kth smallest number is: " + result_int);
+        
         System.out.println();
+
+        //
+
     }//public
     
 }
@@ -90,4 +152,19 @@ class ListNode
         this.value = value;
         this.next = null;
     }
+}
+
+class Node
+{
+    int row;
+    int colum;
+    int value;
+    
+    Node(int row, int colum, int value)
+    {
+        this.row = row;
+        this.colum = colum;
+        this.value = value;
+    }//
+
 }
--- ReverseLinkedList.java
+++ ReverseLinkedList.java
@@ -22,23 +22,94 @@ public class ReverseLinkedList {
         return head;
     }
 
+    public static ListNode reverseSubList(ListNode head, int p, int q)
+    {
+        //check validity
+        if(head == null)
+        {
+            return null;
+        }
+
+        //Find the position of the node of value p
+        ListNode pre = null, current = head, rear = null;
+        while(current.value != p)
+        {
+            pre = current;
+            current = current.next;
+        }
+    
+
+        //Reverse the nodes of values ranging from p to q
+        ListNode temp = null;
+        while(current != null && current.value != q)
+        {
+            temp = current.next;
+            current.next = pre.next;
+            pre.next = current;
+            if(rear == null)
+            {
+                rear = current;
+                rear.next = null;
+            }
+            current = temp;
+            temp = null; 
+        }
+
+        //connect with the remaining part of the original list
+        ListNode result = null;
+        if(current == null)
+        {
+            result = null;
+        }
+        else 
+        {
+            temp = current.next;
+            current.next = pre.next;
+            pre.next = current;
+            rear.next = temp;
+            rear = null;
+            result = head;
+        }
+        
+        //return
+        return head;
+
+    }
+
     public static void main(String[] args) {
 
         // Reverse a linkedlist
+        System.out.print("Nodes of the reversed LinkedList are: ");
         ListNode head = new ListNode(2);
         head.next = new ListNode(4);
         head.next.next = new ListNode(6);
         head.next.next.next = new ListNode(8);
         head.next.next.next.next = new ListNode(10);
-        System.out.print("Nodes of the reversed LinkedList are: ");
         head = reverseList(head);
-        ListNode result = head;
-        while (result != null) {
-            System.out.print(result.value + " ");
-            result = result.next;
+        ListNode p = head;
+        while (p != null) {
+            System.out.print(p.value + " ");
+            p = p.next;
         }
         System.out.println();
 
+
+        //reverse sub-list
+        System.out.print("Nodes of the reversed subList are: ");
+        head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        head = ReverseLinkedList.reverseSubList(head, 2, 4);
+        p = head;
+        while(p != null)
+        {
+            System.out.print(p.value + " ");
+            p = p.next;
+        }
+        System.out.println();
         
     }
 
