5643a4c428b9affc39cde2f4a188b90818c9379e
ztjnwu <ztj_nwu@snnu.edu.cn>
2022-11-18 19:52:55 +0000

    11.18

--- ModifiedBinarySearch.java
+++ ModifiedBinarySearch.java
@@ -2,22 +2,23 @@ import java.util.*;
 
 
 public class ModifiedBinarySearch {
-    static int search(int[] arr, int key)
+    public static int searchNumber(int[] arr, int key)
     {
         if(arr == null)
         {
             return -1;
         }
 
-        //
+        //Initialization
         int start = 0, end = arr.length - 1;
         boolean flag = false; //true stands for ascending, while false stands for descending
         if(arr[end] > arr[start])
         {
             flag = true;
         }
-        int result = -1;
 
+        //Binary Search 
+        int result = -1;
         while(start <= end)
         {
             int middle = start + (end - start) / 2; // must compute the value of middle in this way, otherwise middle = (start + end ) / 2 might lead to overflow.
@@ -56,15 +57,74 @@ public class ModifiedBinarySearch {
         return result;
     }
     
+    public static int searchCeiling(int[] arr, int key)
+    {
+        if(arr == null)
+        {
+            return -1;
+        }
+
+        //Initialization
+        int start = 0, end = arr.length - 1;
+        int middle = start + (end - start) / 2;
+        
+        //Search the given key
+        int result = -1;
+        while(start <= end)
+        {
+            if(key == arr[middle])
+            {
+                result = middle;
+                break;
+            }//
+            else if(key < arr[middle])
+            {
+                end = middle - 1;
+                middle = start + (end - start) / 2;
+            }//
+            else 
+            {
+                start = middle + 1;
+                middle = start + (end - start) / 2;
+            }//
+            
+        }//
+        
+        //update result
+        //Special condition 1
+        if(start == arr.length)
+        {
+            result = -1;
+        }
+
+        //Special condition 2
+        if(end == -1)
+        {
+            result = 0;
+        }
+
+        //return
+        return result;
+
+    }//
 
     public static void main(String[] args)
     {
+        //
         System.out.println("find the index of a given number");
-        System.out.println(ModifiedBinarySearch.search(new int[] { 4, 6, 10 }, 10));
-        System.out.println(ModifiedBinarySearch.search(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 5));
-        System.out.println(ModifiedBinarySearch.search(new int[] { 10, 6, 4 }, 10));
-        System.out.println(ModifiedBinarySearch.search(new int[] { 10, 6, 4 }, 4));
-        System.out.println(ModifiedBinarySearch.search(new int[] { 10, 6, 4 , 1}, 1));
+        System.out.println(ModifiedBinarySearch.searchNumber(new int[] { 4, 6, 10 }, 10));
+        System.out.println(ModifiedBinarySearch.searchNumber(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 5));
+        System.out.println(ModifiedBinarySearch.searchNumber(new int[] { 10, 6, 4 }, 10));
+        System.out.println(ModifiedBinarySearch.searchNumber(new int[] { 10, 6, 4 }, 4));
+        System.out.println(ModifiedBinarySearch.searchNumber(new int[] { 10, 6, 4 , 1}, 1));
+
+        //
+        System.out.println("find the ceiling of a given number");
+        System.out.println(ModifiedBinarySearch.searchCeiling(new int[] { 4, 6, 10 }, 6));
+        System.out.println(ModifiedBinarySearch.searchCeiling(new int[] { 1, 3, 8, 10, 15  }, 15));
+        System.out.println(ModifiedBinarySearch.searchCeiling(new int[] { 4, 6, 10 }, 17));
+        System.out.println(ModifiedBinarySearch.searchCeiling(new int[] { 4, 6, 10  }, -1));
+
     }
 
     
