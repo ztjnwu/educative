f4ae3a2df0e8b349990cf6ea4554330cabbda6d9
ztjnwu <ztj_nwu@snnu.edu.cn>
2023-01-09 21:20:21 +0000

    01.09

--- Subsets.java
+++ Subsets.java
@@ -237,6 +237,94 @@ public class Subsets {
         return result;
     }// 
 
+
+    public static List<Integer> differentExpression(String input)
+    {
+        //Case1
+        if(input == null)
+        {
+            return null;
+        }
+
+        //Case2
+        if(!input.contains("+") && !input.contains("-") && !input.contains("*"))
+        {
+            List<Integer> result = new ArrayList<>();
+            result.add(Integer.parseInt(input));
+            return result;
+        }//
+
+        //Case3
+        List<Integer> result = new ArrayList<>();
+        for(int i = 0; i < input.length(); i++)
+        {
+            if(!Character.isDigit(input.charAt(i)))
+            {
+                List<Integer> left, right;
+                left = differentExpression(input.substring(0, i));
+                right = differentExpression(input.substring(i + 1, input.length()));
+                for(Integer l : left)
+                {
+                    for(Integer r : right)
+                    {
+                        if(input.charAt(i) == '+')
+                        {
+                            result.add(l + r);
+                        }//
+                        else if(input.charAt(i) == '-')
+                        {
+                            result.add(l - r);
+                        }//
+                        else if(input.charAt(i) == '*')
+                        {
+                            result.add(l * r);
+                        }// else
+
+                    }// for
+
+                }// for
+
+            }// for
+        }//
+        return result;
+    }//
+
+
+    public static List<TreeNode> buildBST(int start, int end)
+    {
+        //Case1
+        if(start > end)
+        {
+            List<TreeNode> result = new ArrayList<>();
+            result.add(null);
+            return result;
+        }
+
+        //Case2
+        List<TreeNode> result = new ArrayList<>();
+        for(int i = start; i <= end; i++)
+        {
+            //
+            List<TreeNode> leftTree, rightTree;
+            leftTree = buildBST(start, i - 1);
+            rightTree = buildBST(i + 1, end);
+            
+            //Concanate the left tree and right tree
+            for(TreeNode itemLeft : leftTree)
+            {
+                for(TreeNode itemRight : rightTree)
+                {
+                    TreeNode root = new TreeNode(i);
+                    root.left = itemLeft;
+                    root.right = itemRight;
+                    result.add(root);
+                }//
+
+            }//
+            
+        }//
+        return result;
+    }
     
     public static void main(String[] args)
     {
@@ -262,11 +350,129 @@ public class Subsets {
         System.out.println();
 
         //Balance parenthesis
+        System.out.println(" Balance parenthesis");
         List<String> result_string = Subsets.findParenthesisBalanced(2);
         System.out.println("All combinations of balanced parentheses are: " + result_string);
     
         result_string = Subsets.findParenthesisBalanced(3);
         System.out.println("All combinations of balanced parentheses are: " + result_string);
+
+        //Different expression
+        System.out.println(" Different expression ");
+        List<Integer> result = Subsets.differentExpression("1+2*3");
+        System.out.println("Expression evaluations: " + result);
+    
+        result = Subsets.differentExpression("2*3-4-5");
+        System.out.println("Expression evaluations: " + result);
+
+        //Build unique BST tree
+        System.out.println(" Different BST tree ");
+        List<TreeNode> result_tree = Subsets.buildBST(1, 10);
+        System.out.println(result_tree.size());
+
     }// main
     
 }//Class
+
+
+
+class BST {
+    private TreeNode root;
+
+    // Construction function
+    BST(int[] arr) {
+        this.root = create(arr);
+    }
+
+    public TreeNode getRoot() 
+    {
+        return this.root;
+    }
+
+    // Create a BST tree
+    private TreeNode create(int[] arr)
+    {
+        if (arr == null) 
+        {
+            return null;
+        }
+
+        TreeNode root = null;
+        for (int i = 0; i < arr.length; i++)
+        {
+            root = insert(root, arr[i]);
+        }
+        // return
+
+        return root;
+    }
+
+    // Insert nodes into a BST
+    private TreeNode insert(TreeNode root, int x) 
+    {
+        // the proper postion to be insertednull
+        if (root == null) 
+        {
+            return new TreeNode(x);
+        } // if
+
+        // Select a branch to insert a given node into the tree
+        if (x < root.value) 
+        {
+            root.left = insert(root.left, x);
+        }
+        else if(x > root.value)
+        {
+            root.right = insert(root.right, x);
+        }
+
+        // return
+        return root;
+
+    }//
+
+    // Traverse a BST in preorder
+    public void preOrder(TreeNode root) 
+    {
+        if (root == null) 
+        {
+            return;
+        }
+
+        // traverse
+        System.out.print(root.value + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+
+    }//
+
+    // Traverse a BST in inorder
+    public void inOrder(TreeNode root) 
+    {
+        if (root == null) 
+        {
+            return;
+        }
+
+        // traverse
+        inOrder(root.left);
+        System.out.print(root.value + " ");
+        inOrder(root.right);
+
+    }//
+
+    // Traverse a BST in postOrder
+    public void postOrder(TreeNode root) 
+    {
+        if (root == null) 
+        {
+            return;
+        }
+
+        // traversr
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.value + " ");
+    }//
+
+}//
